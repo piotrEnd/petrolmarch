f6d5e07305b2dddc2d241170c3e983cebe9d8f05
piotrEnd <piotrend@gmail.com>
2022-04-11 06:32:47 +0200

    Updates

--- src/components/AxisLeft.js
+++
@@ -1,9 +0,0 @@
-export const AxisLeft = ({ yScale, innerWidth, tickOffset = 3 }) =>
-  yScale.ticks().map((tickValue, i) => (
-    <g className="tick" transform={`translate(0,${yScale(tickValue)})`} key={i}>
-      <line x2={innerWidth} />
-      <text key={tickValue} style={{ textAnchor: "end" }} x={-tickOffset} dy=".32em">
-        {tickValue} &euro;
-      </text>
-    </g>
-  ))
